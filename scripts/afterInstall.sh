41092eb7445b320cda8c3e73cbed98f6689d45fb
Tõnis Tammeorg <tonis.tammeorg@teleplan.com>
2019-06-03 16:02:10 +0300

    test deploy

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-SW_NAME
 DESTINATION_FOLDER="/var/test_upgrade/home/test_upgrade/"
 SOURCE_FOLDER="/test_upgrade/"
 
@@ -8,9 +7,9 @@ SOURCE_FOLDER="/test_upgrade/"
 
 replaceSoftware()
 {
-    mkdir -p "$DESTINATION_FOLDER/$APPLICATION_NAME";
-    rm rf "$DESTINATION_FOLDER/$APPLICATION_NAME";
-    cp -rf "$SOURCE_FOLDER/$APPLICATION_NAME" "$SOURCE_FOLDER/$APPLICATION_NAME";   
+    rm -rf "$DESTINATION_FOLDER/";
+    mkdir -p "$DESTINATION_FOLDER";
+    cp -rf "$SOURCE_FOLDER/*" "$DESTINATION_FOLDER/";   
     return $?;
 }
 
