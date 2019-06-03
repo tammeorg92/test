d5fbc804d01d9783ae8b43edea41cb4adf8e2c3f
Tõnis Tammeorg <tonis.tammeorg@teleplan.com>
2019-06-03 16:17:49 +0300

    Script update

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DESTINATION_FOLDER="/var/test_upgrade/home/test_upgrade/"
-SOURCE_FOLDER="/test_upgrade/"
+DESTINATION_FOLDER="/var/test_upgrade/home/test_upgrade"
+SOURCE_FOLDER="/test_upgrade"
 
 
 
 replaceSoftware()
 {
-    rm -rf "$DESTINATION_FOLDER/";
+    rm -rf "$DESTINATION_FOLDER";
     mkdir -p "$DESTINATION_FOLDER";
-    cp -rf "$SOURCE_FOLDER/*" "$DESTINATION_FOLDER/";   
+    cp -rf "$SOURCE_FOLDER/*" "$DESTINATION_FOLDER";   
     return $?;
 }
 
