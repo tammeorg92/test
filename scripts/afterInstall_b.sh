f5b1549b2ac13726ee84e0639e54e991d692fb21
Tõnis Tammeorg <tonis.tammeorg@teleplan.com>
2019-06-18 15:41:08 +0300

    new stuff added

--- scripts/afterInstall_b.sh
+++ scripts/afterInstall_b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DESTINATION_FOLDER="/var/nest_upgrade/home/nest_upgrade/nestSW/"
-SOURCE_FOLDER="/nestSW"
+DESTINATION_FOLDER="/var/mpt_upgrade/home/mpt_upgrade/mptSW/"
+SOURCE_FOLDER="/mptSW"
 
 
 
@@ -11,11 +11,11 @@ replaceSoftware()
 {
     mkdir -p "$DESTINATION_FOLDER";
     cp -rf "$SOURCE_FOLDER/." "$DESTINATION_FOLDER";   
-    return 0;
+    return $?;
 }
 
 replaceSoftware; 
-exit 0;
+exit $?;
 
 
 
